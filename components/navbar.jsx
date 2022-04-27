d1debf88c8b223175c6555e1f1cf15f8c09b564a
etsploit <etsploit@protonmail.ch>
2022-04-27 05:30:43 -0400

    started some styling

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -12,46 +12,22 @@ export const Navbar = () => {
 
   return (
     <>
-      <nav className='flex flex-wrap items-center p-3 '>
+      <nav className='flex flex-wrap items-center p-3 py-4 bg-orange-300'>
         <Link href='/'>
-          <a className='inline-flex items-center p-2 mr-4 '>
-            <svg
-              viewBox='0 0 24 24'
-              xmlns='http://www.w3.org/2000/svg'
-              className='w-8 h-8 mr-2 fill-current'
-            >
-              <path d='M12.001 4.8c-3.2 0-5.2 1.6-6 4.8 1.2-1.6 2.6-2.2 4.2-1.8.913.228 1.565.89 2.288 1.624C13.666 10.618 15.027 12 18.001 12c3.2 0 5.2-1.6 6-4.8-1.2 1.6-2.6 2.2-4.2 1.8-.913-.228-1.565-.89-2.288-1.624C16.337 6.182 14.976 4.8 12.001 4.8zm-6 7.2c-3.2 0-5.2 1.6-6 4.8 1.2-1.6 2.6-2.2 4.2-1.8.913.228 1.565.89 2.288 1.624 1.177 1.194 2.538 2.576 5.512 2.576 3.2 0 5.2-1.6 6-4.8-1.2 1.6-2.6 2.2-4.2 1.8-.913-.228-1.565-.89-2.288-1.624C10.337 13.382 8.976 12 6.001 12z' />
-            </svg>
-            <span className='text-xl font-bold tracking-wide uppercase'>
-              Something
-            </span>
+          <a className='inline-flex items-center p-3 mr-4 '>
+          <img
+                        src="https://raw.githubusercontent.com/Mango-Heroes/mh-website/6b18dcb1d537ca84de21c17e52e91b9568222846/template/public/images/mango_logo.png?token=GHSAT0AAAAAABSQCHQUADBKQLZOFTCVQUN4YTI6XZA"
+                        className="h-20 p-2 hover:bg-white hover:bg-opacity-10 rounded"
+                    />
           </a>
         </Link>
-        <Link href='#'>
+        <Link href='https://www.mangoheroes.com/'>
             <button className='font-bold tracking-wide uppercase'>
-              Link1
+              Home
             </button>
           
         </Link>
-        <button
-          className='inline-flex p-3 ml-auto rounded outline-none  lg:hidden'
-          onClick={handleClick}
-        >
-          <svg
-            className='w-6 h-6'
-            fill='none'
-            stroke='currentColor'
-            viewBox='0 0 24 24'
-            xmlns='http://www.w3.org/2000/svg'
-          >
-            <path
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              strokeWidth={2}
-              d='M4 6h16M4 12h16M4 18h16'
-            />
-          </svg>
-        </button>
+
         {/*Note that in this div we will use a ternary operator to decide whether or not to display the content of the div  */}
         <div
           className={`${
@@ -60,8 +36,11 @@ export const Navbar = () => {
         >
           <div className='flex flex-col items-start w-full lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto lg:items-center lg:h-auto'>
           <WalletMultiButton />
+          
           </div>
+          
         </div>
+        
       </nav>
     </>
   );
